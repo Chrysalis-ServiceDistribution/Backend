7c1d2d45457d4bf36ad2143f669e14206f672a67
User Name <erikilla.pinzon@email.com>
2024-05-18 11:13:49 -0400

    update serializers

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,59 +1,52 @@
 from rest_framework import serializers
-from .models import Service, Task, FormField, RequestField, StatusChoices, FieldType
 from django.contrib.auth.models import User
-
+from .models import Service, Task, FormField, RequestField, StatusChoices, FieldType
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
-        extra_kwargs = {'password':{'write_only': True}}
-
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-      user = User.objects.create_user(
-        email=validated_data['email'],
-        password=validated_data['password'],
-        username=validated_data['username']
-      )
-
-      return user
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            username=validated_data['username']
+        )
+        return user
 
 class ServiceSerializer(serializers.ModelSerializer):
-  user =UserSerializer(read_only=True)
-  class Meta:
-    model = Service
-    fields = '__all__'
-
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Service
+        fields = '__all__'
 
 class TaskSerializer(serializers.ModelSerializer):
-   service = ServiceSerializer(read_only=True)
-   client = UserSerializer(read_only=True)
-   status = serializers.ChoiceField(choices=StatusChoices.choices, default=StatusChoices.PENDING)
+    service = ServiceSerializer(read_only=True)
+    client = UserSerializer(read_only=True)
+    status = serializers.ChoiceField(choices=StatusChoices.choices, default=StatusChoices.PENDING)
 
-   class Meta:
-      model = Task
-      fields = '__all__'
-  
+    class Meta:
+        model = Task
+        fields = '__all__'
 
-class FormfieldSerializer(serializers.ModelSerializer):
-   service = ServiceSerializer(read_only=True)
-   type = serializers.ChoiceField(choices=FieldType.choices)
-   choice = serializers.JSONField(required=False)
-   
-   class Meta:
-    model = FormField
-    fields = '__all__'
+class FormFieldSerializer(serializers.ModelSerializer):
+    service = ServiceSerializer(read_only=True)
+    type = serializers.ChoiceField(choices=FieldType.choices)
+    choices = serializers.JSONField(required=False)
 
+    class Meta:
+        model = FormField
+        fields = '__all__'
 
 class RequestFieldSerializer(serializers.ModelSerializer):
-  task = TaskSerializer(serializers.ModelSerializer)
-  type =  serializers.ChoiceField(choices=FieldType.choices)
-  value = serializers.CharField(requiered=False)
-  options = serializers.JSONField(required=False)
-  class Meta:
-    model = RequestField
-    fields = '__all__'
+    task = TaskSerializer(read_only=True)
+    type = serializers.ChoiceField(choices=FieldType.choices)
+    value = serializers.CharField(required=False)
+    options = serializers.JSONField(required=False)
 
-
-
+    class Meta:
+        model = RequestField
+        fields = '__all__'
